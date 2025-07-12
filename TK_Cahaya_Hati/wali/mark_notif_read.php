4267baccb6700551938dddb87efe7f1e93ab2c12
RobertDace <113267580+RobertDace@users.noreply.github.com>
2025-07-12 21:40:44 +0800

    Delete TK_Cahaya_Hati directory

--- TK_Cahaya_Hati/wali/mark_notif_read.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config.php';
-session_start();
-
-if (isset($_SESSION['wali_id'])) {
-    $id_wali = $_SESSION['wali_id'];
-    $conn->query("UPDATE notifikasi SET status_baca = 1 WHERE role_user = 'wali' AND id_user = $id_wali AND status_baca = 0");
-}
-?>
